ac39f219baaedf71a2943d5391ce3db800074791
Leonardo Bressan <leobressan@gmail.com>
2011-03-11 17:21:06 +0100

    Quake data structure

--- src/com/lbb/earthquake/Quake.java
+++ src/com/lbb/earthquake/Quake.java
@@ -1,12 +1,87 @@
 /**
- * 
+ * Quake data structure
  */
 package com.lbb.earthquake;
+
+import java.util.Date;
+import android.location.Location;
 
 /**
  * @author Leonardo Bressan
  *
  */
 public class Quake {
+	/**
+	 * Earthquake date 
+	 */
+	private Date date;
+	/**
+	 * Earthquake magnitude
+	 */
+	private double magnitude;
+	
+	/**
+	 * Earthquake details
+	 */
+	private String details;
+	/**
+	 * Earthquake link
+	 */
+	private String link;
+	/**
+	 * Earthquake location
+	 */
+	private Location location;
+	
+	/**
+	 * Constructor of a Quake object
+	 * @param date - the Earthquake date
+	 * @param magnitude - the Earthquake magnitude
+	 * @param details - the Earthquake details
+	 * @param link - the Earthquake link
+	 * @param location - the Earthquake location
+	 */
+	public Quake(Date date, double magnitude, 
+			String details, String link, Location location) {
+		this.date = date;
+		this.magnitude = magnitude;
+		this.details = details;
+		this.link = link;
+		this.location = location;
+	}
 
+	/**
+	 * @return the Earthquake date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @return the Earthquake magnitude
+	 */
+	public double getMagnitude() {
+		return magnitude;
+	}
+
+	/**
+	 * @return the Earthquake details
+	 */
+	public String getDetails() {
+		return details;
+	}
+
+	/**
+	 * @return the Earthquake link
+	 */
+	public String getLink() {
+		return link;
+	}
+
+	/**
+	 * @return the Earthquake location
+	 */
+	public Location getLocation() {
+		return location;
+	}
 }
